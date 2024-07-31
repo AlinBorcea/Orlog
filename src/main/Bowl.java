da05ad94ced129668600a3713e3e00cc3614e9b9
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2024-07-31 10:06:54 +0300

    implemented generateDieFaces

--- src/main/Bowl.java
+++ src/main/Bowl.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Bowl {
-    private List<DieFace> dice = new ArrayList<>(6);
+    private final List<DieFace> dice = new ArrayList<>(6);
     private int diceLeft = 6;
 
     public List<DieFace> getDice() {
         return dice;
     }
 
-    public void generateDieFaces() {}
+    public void generateDieFaces() {
+        if (diceLeft <= 0) return;
+
+        IntStream nums = new Random().ints(diceLeft, 1, 6);
+        nums.forEach(value -> dice.add(DieFace.fromInt(value)));
+    }
 
 }
