76517cc3b3f88367473790423f4a26e333aecfb6
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2024-07-31 11:28:02 +0300

    created reorder strategy

---
+++ src/main/DefenseFirstStrategy.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.List;
+
+public class DefenseFirstStrategy implements ReorderStrategy {
+    @Override
+    public void reorder(List<DieFace> dice) {
+        List<DieFace> axes = dice.stream().filter(dieFace -> dieFace == DieFace.AX).toList();
+        List<DieFace> arrows = dice.stream().filter(dieFace -> dieFace == DieFace.ARROW).toList();
+        List<DieFace> helmets = dice.stream().filter(dieFace -> dieFace == DieFace.HELMET).toList();
+        List<DieFace> shields = dice.stream().filter(dieFace -> dieFace == DieFace.SHIELD).toList();
+        List<DieFace> hands = dice.stream().filter(dieFace -> dieFace == DieFace.HAND).toList();
+
+        dice.clear();
+        dice.addAll(helmets);
+        dice.addAll(shields);
+        dice.addAll(axes);
+        dice.addAll(arrows);
+        dice.addAll(hands);
+    }
+}
---
+++ src/main/ReorderStrategy.java
@@ -0,0 +1,7 @@
+package main;
+
+import java.util.List;
+
+public interface ReorderStrategy {
+    void reorder(List<DieFace> dice);
+}
