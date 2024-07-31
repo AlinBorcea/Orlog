6234326f9a2e5bb68ead2b7476cf579065b0e5c0
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2024-07-31 11:28:20 +0300

    tested table class

---
+++ src/test/java/TestTable.java
@@ -0,0 +1,26 @@
+package test.java;
+
+import main.AttackFirstStrategy;
+import main.Bowl;
+import main.DefenseFirstStrategy;
+import main.Table;
+import org.junit.jupiter.api.Test;
+
+public class TestTable {
+    @Test
+    public void testTable() {
+        Table table = new Table(new AttackFirstStrategy());
+        Table table2 = new Table(new DefenseFirstStrategy());
+        Bowl bowl = new Bowl();
+
+        bowl.generateDieFaces();
+        table.getDice().addAll(bowl.getDice());
+        table.reorder();
+        table2.getDice().addAll(bowl.getDice());
+        table2.reorder();
+
+        System.out.println(bowl.getDice());
+        System.out.println(table.getDice());
+        System.out.println(table2.getDice());
+    }
+}
